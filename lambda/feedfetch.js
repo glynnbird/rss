65a418335c71143eb30cdd4304c6036365495fc3
Daniel Mermelstein <danmermel@gmail.com>
2022-04-26 15:02:33 +0100

    turned feedfetch into a lambda function deployed by TF

--- lambda/feedfetch.js
+++ lambda/feedfetch.js
@@ -1,16 +1,25 @@
 const Parser = require('rss-parser');
 const parser = new Parser();
+const TABLE = process.env.TABLE
 
-const handler = async function (opts) {
-  const spec = opts.queryStringParameters // GET arguments
+const handler = async function (spec) {
   let feed = await parser.parseURL(spec.url);
   console.log(feed.title);
   const cutoffDate = new Date(spec.date).getTime()
-
   feed.items.forEach(item => {
     const ts = new Date(item.isoDate)
     if (ts.getTime() > cutoffDate) {
       console.log(item)
+
+      try {
+        debug('postChoir write choir', doc)
+        doc.pk = `choir#${doc.choirId}`
+        doc.sk = '#profile'
+        const req = {
+          TableName: aws.TABLE,
+          Item: doc
+        }
+        await aws.documentClient.put(req).promise()
     }
   });
 }
---
+++ terraform/lambda.tf
@@ -0,0 +1,26 @@
+
+//zip up all js files for the pipeline
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_dir = "../lambda"
+  output_path = "../lambda.zip"
+}
+
+
+resource "aws_lambda_function" "feedFetch" {
+  filename  = "../lambda.zip"
+  function_name    = "feedfetch"
+  role             = aws_iam_role.rssLambdaRole.arn
+  handler          = "feedfetch.handler"
+  runtime          = "nodejs14.x"
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+//  layers           = [aws_lambda_layer_version.choirlessAPILambdaLayer.arn]
+
+  environment {
+    variables = {
+      TABLE                  = aws_dynamodb_table.rssDB.name
+    }
+  }
+#   tags = var.tags
+
+}
