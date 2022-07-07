a9b977dcd6e3d682686319e71372c2619f1cabbd
Daniel Mermelstein <danmermel@gmail.com>
2022-07-07 09:44:49 +0100

    deleted old frontend

--- frontend/composables/useConfig.ts
+++
@@ -1,8 +0,0 @@
-import config from "../config.json"
-
-export default function () {
-    return useState('config', function () {
-        console.log("returning this config", config)
-        return config
-    })
-}
--- frontend/composables/useProfile.ts
+++
@@ -1,7 +0,0 @@
-import localstorage from "~/assets/js/localstorage";
-
-export default function () {
-    return useState('profile', function () {
-        return localstorage.loadProfile()
-    })
-}
