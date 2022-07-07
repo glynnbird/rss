b07c83b45c35bd90172aa75878a2e7c341003632
Daniel Mermelstein <danmermel@gmail.com>
2022-07-07 10:02:14 +0100

    nav bar added

--- frontend/pages/newsfeed.vue
+++ frontend/pages/newsfeed.vue
@@ -4,27 +4,35 @@
     <v-card outlined v-bind:key="article.articleid" v-for="article in articles">
       <v-card-title>{{ article.title }}</v-card-title>
       <v-card-text v-html="article.content"> </v-card-text>
+      <v-card-actions>
+        <v-btn :href="article.link" target="_new"
+          color="deep-purple lighten-2"
+          text
+        >
+          Read
+        </v-btn>
+      </v-card-actions>
     </v-card>
   </div>
 </template>
 
 
 <script>
-const config = require ("../config.json")
+const config = require("../config.json");
 export default {
   data: function () {
     return {
-      articles:[]
+      articles: [],
     };
   },
   async asyncData({ store, $axios }) {
-    const profile = store.state.profile.profile
+    const profile = store.state.profile.profile;
     console.log("asyncdata profile is", profile);
     const url = `${config.articlesFunctionUrl.value}?apikey=${profile.apikey}`;
-    const articles = await $axios.$get(url)
+    const articles = await $axios.$get(url);
     //console.log(articles)
 
-    return {articles };
+    return { articles };
   },
 
   methods: {},
