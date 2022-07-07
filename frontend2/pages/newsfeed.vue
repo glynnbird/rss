ded8872b75ebba8228c6aaf5d3da3f4682bf1c9a
Daniel Mermelstein <danmermel@gmail.com>
2022-07-07 09:43:43 +0100

    basic newsfeed page working

---
+++ frontend2/pages/newsfeed.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    Your articles
+    <v-card outlined v-bind:key="article.articleid" v-for="article in articles">
+      <v-card-title>{{ article.title }}</v-card-title>
+      <v-card-text v-html="article.content"> </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+
+<script>
+const config = require ("../config.json")
+export default {
+  data: function () {
+    return {
+      articles:[]
+    };
+  },
+  async asyncData({ store, $axios }) {
+    const profile = store.state.profile.profile
+    console.log("asyncdata profile is", profile);
+    const url = `${config.articlesFunctionUrl.value}?apikey=${profile.apikey}`;
+    const articles = await $axios.$get(url)
+    //console.log(articles)
+
+    return {articles };
+  },
+
+  methods: {},
+};
+</script>
