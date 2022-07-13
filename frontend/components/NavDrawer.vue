8268ecb071116dd491b3d7b2a1295ad028ccb6f2
Daniel Mermelstein <danmermel@gmail.com>
2022-07-13 09:00:35 +0100

    added navigation bar

---
+++ frontend/components/NavDrawer.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-navigation-drawer v-model="show" app>
+    <v-list nav dense>
+
+      <v-list-item @click="onHome">
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Home</v-list-item-title>
+      </v-list-item>
+
+      <v-list-item @click="onFeedList">
+        <v-list-item-icon>
+          <v-icon>mdi-format-list-bulleted</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>List Feeds</v-list-item-title>
+      </v-list-item>
+
+      <v-list-item @click="onFeedAdd">
+        <v-list-item-icon>
+          <v-icon>mdi-rss</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Add Feed</v-list-item-title>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list-item @click="onLogout">
+        <v-list-item-icon>
+          <v-icon>mdi-logout</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Log out</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    onHome: function () {
+      this.$router.push("/");
+    },
+    onFeedList: function () {
+      this.$router.push("/showfeeds");
+    },
+    onFeedAdd: function () {
+      this.$router.push("/addfeed");
+    },
+    onLogout: function () {
+      this.$router.push("/logout");
+    },
+  },
+};
+</script>
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
   <v-app>
+    <NavDrawer ref="drawer" />
     <v-app-bar app class="light-blue">
+      <v-app-bar-nav-icon
+        @click="$refs.drawer.show = !$refs.drawer.show"
+      ></v-app-bar-nav-icon>
       <v-toolbar-title @click="onHome">
         <span class="apptitle">RSS Wrangler</span>
       </v-toolbar-title>
