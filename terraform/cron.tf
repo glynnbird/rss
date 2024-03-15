331c0d3fd77f6b3ec3d72a9e373198be0c89b011
Glynn Bird <glynn.bird@gmail.com>
2024-03-15 15:54:38 +0000

    poll every 3 hours

--- terraform/cron.tf
+++ terraform/cron.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_event_rule" "runFeedDispatcher" {
     name = "runFeedDispatcher"
     description = "Fires every five minutes to poll each feed"
-    schedule_expression = "rate(60 minutes)"
+    schedule_expression = "rate(180 minutes)"
 }
 
 resource "aws_cloudwatch_event_target" "runFeedDispatcherEventTarget" {
