84cb917e403d64f6a083afb5f310af716b58b605
Glynn Bird <glynn.bird@gmail.com>
2024-09-06 10:33:11 +0100

    first stab at Nuxt3 app

---
+++ frontend3/nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+export default defineNuxtConfig({
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    //...
+  ],
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  }
+})
---
+++ frontend3/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+  const router = useRouter()
+  // const sponsor = ref(0)
+  // sponsor.value = ''
+
+  // const viewSponsor = async () => {
+  //   if (sponsor.value.length > 0) {
+  //     router.push({ path: `/sponsor-${sponsor.value}` })
+  //   }
+  // }
+
+</script>
+<template>
+  <h1>Hello World</h1>
+</template>
