ebf7e56e7ef53729a814a7c8daaaf07c3e886a9d
Glynn Bird <glynn.bird@gmail.com>
2024-09-06 16:07:26 +0100

    another go at getting the  manifest to work

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -25,7 +25,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: { 
-      link: [ { rel: 'manifest', href: '/manifest.json'} ]
+      link: [ { rel: 'manifest', href: '/rss.webmanifest'} ]
     }
   }
 })
