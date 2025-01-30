3c6c7866651973aab33aa6fd46c5fc0a7fd17c38
Glynn Bird <glynn.bird@gmail.com>
2025-01-30 16:14:45 +0000

    add image thing back

---
+++ code/image.js
@@ -0,0 +1,45 @@
+import { okResponse, missingResponse, notOkResponse, notOk } from './lib/constants.js'
+import { mustBePOST, mustBeJSON, apiKey, handleCORS } from './lib/checks.js'
+
+const parser = function(str) {
+  // remove everything past the end head
+  let i
+  i = str.search('</head>')
+  str = str.slice(0, i)
+  // break into lines
+  str = str.replace(/</g, '\n')
+  str = str.replace(/\>/g, '\n')
+  // only consider lines containing the twitter image
+  const lines = str.split('\n').filter((l) => { return l.includes('name="twitter:image"') || l.includes('name="twitter:image:src"')})
+  if (lines.length > 0) {
+    const line = lines[0]
+    str = line.replace(/.*content="/, '').replace(/".*/, '')
+    return str
+  }
+  return str
+}
+
+export async function onRequest(context) {
+
+  // handle POST/JSON/apikey chcecks
+  const r = handleCORS(context.request) || apiKey(context.request, context.env) || mustBePOST(context.request) || mustBeJSON(context.request)
+  if (r) return r
+
+  // parse the json
+  const json = await context.request.json()
+  const url = json.url
+  if (!url) {
+    return new Response(notOk, notOkResponse)
+  }
+
+  // if there's a url
+  const fr = await fetch(url)
+  const content = await fr.text()
+  const parsed = parser(content)
+  if (parsed) {
+    return new Response(JSON.stringify({ url: parsed }), okResponse)
+  } else {
+    // send 404 response
+    return new Response(notOk, missingResponse)
+  }
+}
