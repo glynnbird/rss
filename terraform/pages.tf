cf0c48f014413805eb6d4fa533299922a102ba42
Glynn Bird <glynn.bird@gmail.com>
2025-06-26 13:40:43 +0100

    upgrade to terraform provider v5

--- terraform/pages.tf
+++ terraform/pages.tf
@@ -4,38 +4,55 @@ resource "cloudflare_pages_project" "frontend_project" {
   name              = "rss"
   production_branch = "main"
   
-  build_config {
+  build_config = {
     build_command       = "./build.sh"
     destination_dir     = "dist"
     root_dir            = "/"
   }
 
-  source {
+  source = {
     type = "github"
-    config {
+    config = {
       owner                         = "glynnbird"
       repo_name                     = "rss"
       production_branch             = "main"
-      # pr_comments_enabled           = true
-      # deployments_enabled           = true
-      # production_deployment_enabled = true
-      # preview_deployment_setting    = "custom"
-      # preview_branch_includes       = ["dev", "preview"]
-      # preview_branch_excludes       = ["main", "prod"]
     }
   }
-    deployment_configs {
-      preview {
-        
-      }
-      production {
-        environment_variables = {
-          NODE_VERSION = "20"
-          API_KEY = random_string.apiKey.id
+    deployment_configs = {
+      preview = {
+         env_vars = {
+          NODE_VERSION = {
+            type = "plain_text"
+            value = "22"
+          }
+          API_KEY = {
+            type = "plain_text"
+            value = random_string.apiKey.id
+          }
         }
 
         kv_namespaces = {
-          KV = cloudflare_workers_kv_namespace.rsskv.id
+          KV ={
+            namespace_id = cloudflare_workers_kv_namespace.rsskv.id
+          }
+        }
+      }
+      production = {
+        env_vars = {
+          NODE_VERSION = {
+            type = "plain_text"
+            value = "22"
+          }
+          API_KEY = {
+            type = "plain_text"
+            value = random_string.apiKey.id
+          }
+        }
+
+        kv_namespaces = {
+          KV ={
+            namespace_id = cloudflare_workers_kv_namespace.rsskv.id
+          }
         }
       }
   }
@@ -44,10 +61,10 @@ resource "cloudflare_pages_project" "frontend_project" {
 resource "cloudflare_pages_domain" "frontend_domain" {
   account_id   = var.cloudflare_account_id
   project_name = cloudflare_pages_project.frontend_project.name
-  domain       = var.cloudflare_domain
+  name       = var.cloudflare_domain
 }
 
-resource "cloudflare_record" "frontend_dns" {
+resource "cloudflare_dns_record" "frontend_dns" {
   zone_id = var.cloudflare_zone_id
   name    = "rss"
   content   = cloudflare_pages_project.frontend_project.subdomain
