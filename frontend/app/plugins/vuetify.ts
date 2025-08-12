b6707535bad474cf6bcefe0a19f5b5f612a486ac
Glynn Bird <glynn.bird@gmail.com>
2025-08-12 14:16:11 +0100

    new nuxt

--- frontend/app/plugins/vuetify.ts
+++ frontend/app/plugins/vuetify.ts
@@ -1,15 +1,10 @@
-// plugins/vuetify.js
 import '@mdi/font/css/materialdesignicons.css'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    ssr: false,
-    components,
-    directives,
+    ssr: false
   })
   app.vueApp.use(vuetify)
 })
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,15 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
   build: {
     transpile: ['vuetify'],
   },
   modules: [
+    // from https://github.com/vite-pwa/nuxt?tab=readme-ov-file#-usage
     '@vite-pwa/nuxt',
-    (_options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => {
-        // @ts-expect-error
-        config.plugins.push(vuetify({ autoImport: true }))
+    // from https://vuetifyjs.com/en/getting-started/installation/#manual-setup
+    async (options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', config => {
+        if (config && config.plugins) {
+          config.plugins.push(vuetify({ autoImport: true }))
+        }
       })
     }
   ],
@@ -57,6 +61,11 @@ export default defineNuxtConfig({
   vite: {
     define: {
       'process.env.DEBUG': false,
+    },
+    vue: {
+      template: {
+        transformAssetUrls,
+      }
     }
   }
 })
